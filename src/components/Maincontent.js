3da5199b4093df0edb2edc3d4fba6b91ab766ebd
junmok107 <sjmsjm1017@gmail.com>
2024-07-18 16:15:30 +0900

    코드수정

--- src/components/Maincontent.js
+++ src/components/Maincontent.js
@@ -18,17 +18,15 @@ function MainContent() {
       </div>
       <div className="select">
         <label htmlFor="name" className="class_n">닉네임</label>
-        <input type="text" id="name" value={nickname} onChange={(e) => setNickname(e.target.value)} placeholder="ex) 노래하는 김형준" />
+        <input type="text" id="name" value={nickname} onChange={(e) => setNickname(e.target.value)} placeholder="ex) 잘생긴 홍길동" />
       </div>
       <div className="select">
         <label htmlFor="appeal" className="class_n">매력어필</label>
-        <textarea id="appeal" value={appeal} onChange={(e) => setAppeal(e.target.value)} placeholder="김형준 멍청이"></textarea>
+        <textarea id="appeal" value={appeal} onChange={(e) => setAppeal(e.target.value)} placeholder="간단하게 한 줄로 매력을 어필해보세요!"></textarea>
       </div>
       <div className="select">
         <span className="class_n">연락수단</span>
         <div className="radio-group">
-          <input type="radio" id="all" name="contact" value="모두선택" checked={contact === '모두선택'} onChange={() => setContact('모두선택')} />
-          <label htmlFor="all">모두선택</label>
           <input type="radio" id="insta" name="contact" value="인스타" checked={contact === '인스타'} onChange={() => setContact('인스타')} />
           <label htmlFor="insta">인스타</label>
           <input type="radio" id="phone" name="contact" value="전화번호" checked={contact === '전화번호'} onChange={() => setContact('전화번호')} />
